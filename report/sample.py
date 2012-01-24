513405a5c616ee6704ce1ba531b9ed4547593790
Lee Wei <evandrix@gmail.com>
2012-01-24 17:13:43 +0000

    updates

---
+++ report/sample.py
@@ -0,0 +1,11 @@
+class TestClass:	
+	def __init__(self):
+		"""
+		Test Class without arguments
+		"""
+		
+	def TestClassMethod(self):
+		"""
+		TestClass Method without args
+		"""
+		print "TestClassMethod"

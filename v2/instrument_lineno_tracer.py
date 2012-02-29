d3fe3339ff594d3a8b4b134601a9b062fe5e226e
Lee Wei <evandrix@gmail.com>
2012-02-29 15:31:01 +0000

    updates before initial

--- v2/instrument_lineno_tracer.py
+++ v2/instrument_lineno_tracer.py
@@ -7,8 +7,12 @@ import types
 import byteplay
 from headers import *
 
+TOTAL_LINE_COVERAGE=-1
+ACTUAL_LINE_COVERAGE=[]
+
 # @ http://www.dalkescientific.com/writings/diary/archive/2005/04/20/tracing_python_code.html
-def trace(frame, event, arg):    
+def trace(frame, event, arg):
+    global ACTUAL_LINE_COVERAGE
     if event == 'line':
         lineno = frame.f_lineno
         # frame.f_code.co_filename
@@ -18,7 +22,9 @@ def trace(frame, event, arg):
             filename = filename[:-1]
         # frame.f_code.co_name
         name = frame.f_globals["__name__"]
-        line = linecache.getline(filename, lineno)
+        
+        # won't work - missing source code!
+        #line = linecache.getline(filename, lineno)
 
         func_line_no = frame.f_lineno
         co = frame.f_code
@@ -31,11 +37,16 @@ def trace(frame, event, arg):
                 take = arg == func_line_no
             elif take:
                 code_fragment.append(pair)
+        if len(code_fragment) == 1:
+            code_fragment = code_fragment[0]
 
         if name == MODULE_UNDER_TEST:
             print "%s:%s: %s" % (name, lineno, code_fragment)
+            ACTUAL_LINE_COVERAGE.append(code_fragment)
         #print '\t', frame.f_locals
         #print '\t', frame.f_code.co_varnames[:frame.f_code.co_argcount]
+    else:
+        print "event:", event
     return trace
 
 class A(object):
@@ -49,15 +60,46 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         M = __import__(sys.argv[1]) # import module as string
         dis.dis(M)
-        sys.exit(0)
+
+    M = __import__(MODULE_UNDER_TEST)
+    functions = getmembers(M, isfunction)
+    f = [ fn for name,fn in functions if name == 'foo' ][0]
+    c = byteplay.Code.from_code(f.func_code)
+    c.code = [ (op,arg) for op,arg in c.code if op != byteplay.SetLineno ]
+    TOTAL_LINE_COVERAGE = len(c.code)
+    #print "100%% bytecode line coverage = %d instructions" % len(c.code)
+
+    #dis.dis(f.func_code.co_code)
 
     M = __import__(MODULE_UNDER_TEST)
     functions = getmembers(M, isfunction)
     print '---'
     f = [ fn for name,fn in functions if name == 'foo' ][0]
     sys.settrace(trace)
-    f(A(),B(),3,'4')
+    try:
+        f(A(),B(),3,'4')
+        #f(None,B(),None,None)
+    except:
+        pass
     sys.settrace(None)
     print '***'
     #dis.dis(M)
     print '---'
+
+    final_coverage = []
+    omit = False
+    for opcode, arg in ACTUAL_LINE_COVERAGE:
+        if omit and opcode == byteplay.RETURN_VALUE:
+            omit = False
+            continue
+        if opcode == byteplay.CALL_FUNCTION and arg == 1:
+            omit = True
+            continue
+        if not omit:
+            final_coverage.append((opcode, arg))
+    print final_coverage
+            
+    ACTUAL_LINE_COVERAGE = len(final_coverage)
+
+    print "(Byte)code coverage: %d/%d instructions (%.2f%%)" % (ACTUAL_LINE_COVERAGE,TOTAL_LINE_COVERAGE, ACTUAL_LINE_COVERAGE/float(TOTAL_LINE_COVERAGE)*100)
+    sys.exit(0)

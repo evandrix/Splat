5dcb6f00f7c22567839d81d490261326eab5fa34
Lee Wei <evandrix@gmail.com>
2012-03-15 21:16:47 +0000

    added warning to instrument bytecode first before test & raising RTE after max recursion depth reached

--- _new_recursive_functions/recursive_fn_test.py
+++ _new_recursive_functions/recursive_fn_test.py
@@ -11,11 +11,13 @@ import copy
 from decorator import decorator
 from pprint import pprint
 
+# global settings
 sys.setrecursionlimit(2**10)
-TRACE_INTO = []
 TRACE_DICT = 'trace_params'
 
 def update_frame_param(current, event):
+    """ mutate & propagate trace_params down the stack frames """
+
     params = [
         ("num_recursive_calls", lambda parent: parent+1),
         (       "stack_frames", lambda parent: parent+[current]),
@@ -26,8 +28,7 @@ def update_frame_param(current, event):
             parent.f_locals[TRACE_DICT].update({p: fn(parent.f_locals[TRACE_DICT][p])})
         current.f_locals[TRACE_DICT] = parent.f_locals[TRACE_DICT]
     elif event == 'return':
-        for p,_ in params:
-            parent.f_locals[TRACE_DICT][p] = current.f_locals[TRACE_DICT][p]
+        parent.f_locals[TRACE_DICT] = current.f_locals[TRACE_DICT]
     return current
 
 def current_func():
@@ -42,7 +43,7 @@ def current_func():
     frame = inspect.currentframe(1)
     code  = frame.f_code
     globs = frame.f_globals
-    functype = type(lambda: 0)
+    functype = type(lambda: None)
     funcs = []
     for func in gc.get_referrers(code):
         if type(func) is functype:
@@ -67,17 +68,27 @@ def trace(frame, event, arg):
     varnames  = co.co_varnames[:co.co_argcount]
 
     if event == 'line':
+        # pre: bytecode must be instrumented!
         cb = byteplay.Code.from_code(co)
+
         # ...(<Label>, None), (SetLineno, <#>)...
         #   => ...(SetLineno, <#>), (<Label>, None), (SetLineno, <#>)...
         code = []
         for i,(opcode,arg) in enumerate(cb.code):
-            if isinstance(opcode, byteplay.Label):
+            if i < len(cb.code)-1 and isinstance(opcode, byteplay.Label):
                 code.append(cb.code[i+1])
             code.append((opcode,arg))
         cb.code = code
+
+        # pre: assumes bytecode list of the form:
+        # - [ ((SetLineno, <#>), (opcode, arg)) x ... ]
         code_fragments = zip(cb.code[::2],cb.code[1::2])
         code_fragment  = [inst_node for (lineno_node,inst_node) in code_fragments if lineno_node == (byteplay.SetLineno, lineno)]
+
+        indexes = [i for i,(a,b) in enumerate(code_fragments) if a == (byteplay.SetLineno, lineno)]
+        if len(indexes) < 1:
+            raise RuntimeError("Please instrument bytecode under test first.")
+
         next_i = [i for i,(a,b) in enumerate(code_fragments) if a == (byteplay.SetLineno, lineno)][-1] + 1
         if next_i < len(code_fragments)-1:
             _, next_node = code_fragments[next_i]  
@@ -90,11 +101,14 @@ def trace(frame, event, arg):
         #print '0x%x'%id(frame),lineno,offset,code_fragment
     elif event == 'call':
         if name == 'write':        return
-        if name not in TRACE_INTO: return
         update_frame_param(frame, event)
+        if name not in frame.f_locals[TRACE_DICT]['function']: return
         #print >> sys.stderr, '[call]: 0x%x=>0x%x, %s,%s\n%s\n%s' % \
         #    (id(frame.f_back), id(frame), caller.f_lineno,
         #    caller.f_lasti, frame.f_locals, arg)
+        
+        if frame.f_locals[TRACE_DICT]['num_recursive_calls'] > sys.getrecursionlimit():
+            raise RuntimeError("Maximum recursion depth reached")
 
         # potential for branching off different trace functions (!)
         return current_func()
@@ -126,49 +140,50 @@ def trace(frame, event, arg):
             print "self.assertEquals(%r, h.%s(%s))" % (arg,name,param)
     elif event == 'exception':
         exc_type, exc_value, exc_traceback = arg
+        print >> sys.stderr, "Exception event: %s" % arg
     else:
-        print >> sys.stderr, "Unhandled event '%s'" % event
+        print >> sys.stderr, "Unhandled event: '%s'" % event
     return
 
-if __name__ == "__main__":
-    old_dir = __builtins__.dir
-    def new_dir(*args, **kwargs):
-        """ builtin dir() without __var__ clutter """
-        return [a for a in old_dir(*args, **kwargs) if not a.startswith("__")]
-    __builtins__.dir = new_dir
+old_dir = __builtins__.dir
+def new_dir(*args, **kwargs):
+    """ builtin dir() without __var__ clutter """
+    return [a for a in old_dir(*args, **kwargs) if not a.startswith("__")]
 
+if __name__ == "__main__":
+    __builtins__.dir = new_dir
     from factorial import *
     from hanoi     import *
-    h = Hanoi()
-    TRACE_INTO = [ factorial.func_name, h.hanoi.func_name ]
+    h = Hanoi(0x41414141)
     sys._getframe().f_locals[TRACE_DICT] = {
-        
+                   "function": h.hanoi.func_name,
         "num_recursive_calls": 0,
-        "stack_frames": []
+               "stack_frames": []
+    }
+    sys.settrace(trace)
+    try:
+        h.hanoi(25)
+    except Exception as e:
+        print >> sys.stderr, e.__class__.__name__+':', e.message
+        pass
+    sys.settrace(None)
+    print
+    for k,v in sys._getframe().f_locals[TRACE_DICT].items():
+        if k == "stack_frames":
+            v = '<hidden>'#'' => '.join(map(lambda f:'0x%x'%id(f), v))
+        print k+':',v
+    print
+    sys.exit(0)
+    sys._getframe().f_locals[TRACE_DICT] = {
+                   "function": factorial.func_name,
+        "num_recursive_calls": 0,
+               "stack_frames": []
     }
     sys.settrace(trace)
     for i in xrange(3,4,1):
        try:
             factorial(i)
        except Exception as e:
-            print >> sys.stderr, e.__class__.__name__, e.message
+            print >> sys.stderr, e.__class__.__name__+':', e.message
             continue
     sys.settrace(None)
-    print
-    for k,v in sys._getframe().f_locals[TRACE_DICT].items():
-        if k == "stack_frames":
-            v = ' => '.join(map(lambda f:'0x%x'%id(f), v))
-        print k+':',v
-    print
-
-    sys.exit(0)
-    sys._getframe().f_locals[TRACE_DICT] = \
-        { "num_recursive_calls": 0, "stack_frames": [] }
-    sys.settrace(trace)
-    try:
-        h.hanoi(5)
-    except Exception as e:
-        print >> sys.stderr, e.__class__.__name__, e.message
-        pass
-    sys.settrace(None)
-

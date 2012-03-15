71758f305a90bc995c40a27971fae9fc54153bb1
Lee Wei <evandrix@gmail.com>
2012-03-15 20:32:31 +0000

    added function to determine current function (not code) object

--- _new_recursive_functions/trace_hanoi.py
+++ _new_recursive_functions/trace_hanoi.py
@@ -3,6 +3,7 @@ import time
 import dis
 import byteplay
 import inspect
+import gc
 import multiprocessing
 import logging
 import numbers
@@ -28,6 +29,29 @@ def update_frame_param(current, event):
         for p,_ in params:
             parent.f_locals[TRACE_DICT][p] = current.f_locals[TRACE_DICT][p]
     return current
+
+def current_func():
+    """ credits @ http://goo.gl/Um0Ig
+    The stack frame tells us what code object we're in. If we can find a function object that refers to that code object in its func_code attribute, we have found the function.
+
+    Fortunately, we can ask the garbage collector which objects hold a reference to our code object, and sift through those, rather than having to traverse every active object in the Python world. There are typically only a handful of references to a code object.
+
+    Note: functions can share code objects (in the case where you return a function from a function, ie. a closure). When there's more than one function using a given code object, we can't tell which function it is, so we return None.
+    """
+
+    frame = inspect.currentframe(1)
+    code  = frame.f_code
+    globs = frame.f_globals
+    functype = type(lambda: 0)
+    funcs = []
+    for func in gc.get_referrers(code):
+        if type(func) is functype:
+            if getattr(func, "func_code", None) is code:
+                if getattr(func, "func_globals", None) is globs:
+                    funcs.append(func)
+                    if len(funcs) > 1:
+                        return None
+    return funcs[0] if funcs else None
 
 def trace(frame, event, arg):
     # frame
@@ -59,12 +83,11 @@ def trace(frame, event, arg):
             _, next_node = code_fragments[next_i]  
             if next_node[0] in [byteplay.POP_JUMP_IF_TRUE, byteplay.POP_JUMP_IF_FALSE]:
                 code_fragment.append(next_node)
-
         # possible code_fragment elements:
         # - [(opcode, arg)]
         # - [(opcode, arg), (POP_JUMP_IF_*, <label>)]
         # - [(<label>, None), (opcode, arg)]
-        print '0x%x'%id(frame),lineno,offset,code_fragment
+        #print '0x%x'%id(frame),lineno,offset,code_fragment
     elif event == 'call':
         if name == 'write':        return
         if name not in TRACE_INTO: return
@@ -72,7 +95,9 @@ def trace(frame, event, arg):
         #print >> sys.stderr, '[call]: 0x%x=>0x%x, %s,%s\n%s\n%s' % \
         #    (id(frame.f_back), id(frame), caller.f_lineno,
         #    caller.f_lasti, frame.f_locals, arg)
-        return trace    # for tracing lines
+
+        # potential for branching off different trace functions (!)
+        return current_func()
     elif event == 'return':
         update_frame_param(frame, event)
         #print >> sys.stderr, '[return]: 0x%x=>0x%x, %s,%s\n%s\n%s,%s' % \
@@ -88,6 +113,7 @@ def trace(frame, event, arg):
         # factorial
         if isinstance(arg, int) and long(arg) > sys.maxint:
             arg = long(arg)
+        if isinstance(arg, numbers.Number):
             print "self.assertEquals(%d, %s(%s))" % (arg,name,param)
 
         # hanoi
@@ -115,11 +141,11 @@ if __name__ == "__main__":
     from hanoi     import *
     h = Hanoi()
     TRACE_INTO = [ factorial.func_name, h.hanoi.func_name ]
-    
-    dis.dis(factorial)
-
-    sys._getframe().f_locals[TRACE_DICT] = \
-        { "num_recursive_calls": 0, "stack_frames": [] }
+    sys._getframe().f_locals[TRACE_DICT] = {
+        
+        "num_recursive_calls": 0,
+        "stack_frames": []
+    }
     sys.settrace(trace)
     for i in xrange(3,4,1):
        try:

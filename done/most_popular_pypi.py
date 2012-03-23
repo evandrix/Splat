0ef98dee4bbfab959b8a5f61a9a41ab826dfec2c
Lee Wei <evandrix@gmail.com>
2012-03-23 04:45:59 +0000

    duplicated print to console/output file

--- done/most_popular_pypi.py
+++ done/most_popular_pypi.py
@@ -8,21 +8,23 @@ from BeautifulSoup import BeautifulSoup
 BASE_URL   = 'http://pypi.python.org/pypi/'
 SIMPLE_URL = 'http://pypi.python.org/simple/'
 
-r = requests.get(SIMPLE_URL)
-soup = BeautifulSoup(r.content)
-for i, a in enumerate(soup('a')):
-    name = a.contents[0]
-    url = BASE_URL + a.attrs[0][1]
-
-    try:
-        r = requests.get(url)
-        package_soup = BeautifulSoup(r.content)
+with open('packages.txt', 'w') as fout:
+    r = requests.get(SIMPLE_URL)
+    soup = BeautifulSoup(r.content)
+    for i, a in enumerate(soup('a')):
+        name = a.contents[0]
+        url = BASE_URL + a.attrs[0][1]
         try:
-            values = (int(td.contents[0]) for td in package_soup('td', style='text-align: right;') if td.contents and td.contents[0].isdigit())
-        except Exception:
-            values = [-1]
-    except Exception:
-        values = [-2]
-    print sum(values), name
+            r = requests.get(url)
+            package_soup = BeautifulSoup(r.content)
+            try:
+                values = (int(td.contents[0]) for td in package_soup('td', style='text-align: right;') if td.contents and td.contents[0].isdigit())
+            except:
+                values = [-1]
+        except:
+            values = [-2]
+        print sum(values), name
+        print >> fout, sum(values), name
 
-print 'Fetched statistics from %d packages' % (i + 1)
+    print 'Fetched statistics from %d packages' % (i + 1)
+    print >> fout, 'Fetched statistics from %d packages' % (i + 1)
